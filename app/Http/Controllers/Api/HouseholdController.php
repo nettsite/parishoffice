f6672f94dedce946cc42836aeead2f0b2255ed3b
NettSite <william@nettsite.co.za>
2025-08-29 17:36:05 +0200

    Add certificate information retrieval for household members in show and members methods

--- app/Http/Controllers/Api/HouseholdController.php
+++ app/Http/Controllers/Api/HouseholdController.php
@@ -22,6 +22,28 @@ class HouseholdController extends Controller
         Log::debug("[Household] {$action}", $context);
     }
 
+    /**
+     * Get certificate information for a member
+     */
+    private function getMemberCertificateInfo($member, $certificateType)
+    {
+        $collectionName = $certificateType . '_certificates';
+        $media = $member->getFirstMedia($collectionName);
+
+        if (!$media) {
+            return null;
+        }
+
+        return [
+            'media_id' => $media->id,
+            'file_name' => $media->file_name,
+            'url' => url("api/members/{$member->id}/certificates/{$certificateType}/download"),
+            'size' => $media->size,
+            'mime_type' => $media->mime_type,
+            'uploaded_at' => $media->created_at,
+        ];
+    }
+
     public function show(Request $request)
     {
         $household = $request->user();
@@ -30,7 +52,18 @@ class HouseholdController extends Controller
             'household_id' => $household->id
         ]);
 
-        $household->load('members');
+        $household->load(['members' => function($query) {
+            $query->with('media');
+        }]);
+
+        // Add certificate information to each member
+        $household->members->each(function($member) {
+            $member->certificates = [
+                'baptism' => $this->getMemberCertificateInfo($member, 'baptism'),
+                'first_communion' => $this->getMemberCertificateInfo($member, 'first_communion'),
+                'confirmation' => $this->getMemberCertificateInfo($member, 'confirmation'),
+            ];
+        });
 
         $this->logActivity('Household details retrieved', [
             'household_id' => $household->id,
@@ -122,7 +155,16 @@ class HouseholdController extends Controller
         ]);
 
         try {
-            $members = $household->members;
+            $members = $household->members()->with('media')->get();
+
+            // Add certificate information to each member
+            $members->each(function($member) {
+                $member->certificates = [
+                    'baptism' => $this->getMemberCertificateInfo($member, 'baptism'),
+                    'first_communion' => $this->getMemberCertificateInfo($member, 'first_communion'),
+                    'confirmation' => $this->getMemberCertificateInfo($member, 'confirmation'),
+                ];
+            });
 
             $this->logActivity('Members retrieved successfully', [
                 'household_id' => $household->id,
